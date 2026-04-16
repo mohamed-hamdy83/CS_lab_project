981c9c188af14d41dde5b453168735fd150120b3
mohamed-hamdy83 <mh2791461@gmail.com>
2026-04-16 17:51:01 +0200

    added INetworkClient interface for milestone 2

---
+++ include/INetworkClient.h
@@ -0,0 +1,18 @@
+#ifndef INETWORK_CLIENT_H
+#define INETWORK_CLIENT_H
+
+#include <string>
+
+class INetworkClient {
+public:
+    virtual ~INetworkClient() = default;
+
+    
+    virtual bool connectToServer(const std::string& ipAddress, int port) = 0;
+    
+    virtual void disconnect() = 0;
+    
+    virtual void sendJsonMessage(const std::string& jsonPayload) = 0;
+};
+
+#endif
